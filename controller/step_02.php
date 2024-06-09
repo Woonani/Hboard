c26a663b95c78a14a316a7cea4d2bc1afbe08213
Woonani <nada3377@yonsei.ac.kr>
2024-06-10 07:13:34 +0900

    feat:본인확인 수정

--- controller/step_02.php
+++ controller/step_02.php
@@ -2,6 +2,7 @@
 // 디버깅을 위한 로그 파일 설정
 ini_set('log_errors', 1);
 ini_set('error_log', '/data/logs/php-error.log');
+session_start();
 
 print_r($_SERVER['REQUEST_METHOD']);
 
@@ -9,7 +10,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     if($_POST['mode'] == 'phone_auth' && isset($_POST['data'])) { 
         // print_r($_POST['data']); // 들어옴 확인
-        session_start();
         $_SESSION['phone_auth_num'] = '123456';
         $_SESSION['user_phone_num'] = $_POST['data'];
 
@@ -23,16 +23,17 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
    
     } else if ($_POST['mode'] == 'auth_num_chk' && isset($_POST['data'])) { 
         print_r($_POST['data']); // 들어옴 확인
+        print_r($_SESSION['phone_auth_num']); // 들어옴 확인
         
         if($_POST['data'] == $_SESSION['phone_auth_num']){
-            
-            header('Content-Type: application/json');
-            echo json_encode(['status' => 'success', 'code' => '인증 성공']);
-        } 
-        // else {
-        //     header('Content-Type: application/json');
-        //     echo json_encode(['status' => 'fail', 'message' => '인증번호 불일치']);
-        // }
+            print_r("same");
+            // header('Content-Type: application/json');
+            // echo json_encode(['status' => 'success', 'code' => '인증 성공']);
+        } else {
+            print_r("nope!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            // header('Content-Type: application/json');
+            // echo json_encode(['status' => 'fail', 'message' => '인증번호 불일치']);
+        }
 
     } else {
         // 잘못된 요청 처리
--- member/index.php
+++ member/index.php
@@ -5,7 +5,7 @@
 $file = isset($_GET['mode']) ? $_GET['mode'].".html" : '';
 
 if($file && file_exists($file)) {
-    include_once $file;
+    include $file;
 } else {
     // 아래코드는 무한 실행..
     // echo "<script>window.location.href = 'index.php';</script>";
