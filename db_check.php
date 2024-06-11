0d880c35b7ea06d8724532f8908b75e9f0b5f114
woonani <nada3377@naver.com>
2024-06-11 13:52:05 +0900

    db설정 fix

--- db_check.php
+++ db_check.php
@@ -1,20 +1,39 @@
 <?php
-$servername = "172.17.0.2";  // MySQL 호스트 이름
+// phpinfo();
+$hostname = "172.16.1.66";  // 내 pc IP
+// $hostname = "127.0.0.1";  // MySQL 호스트 이름
 $username = "hackers";      // MySQL 사용자 이름
 $password = "hackers1234!"; // MySQL 비밀번호
-$dbname = "Hboard";          // 데이터베이스 이름
+$database = "Hboard";          // 데이터베이스 이름
+$port = "3306";          // 데이터베이스 이름
 
-// MySQL 서버와 소켓 파일 위치 설정
-$socket = "/var/run/mysqld/mysqld.sock";
+
 
 // MySQL 연결 생성
-$conn = new mysqli($servername, $username, $password, $dbname, null, $socket);
+$conn = mysqli_connect($hostname, $username, $password, $database, $port); 
 
-// 연결 확인
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully";
+// Check connection
+if (mysqli_connect_errno()) {
+    echo $hostname."Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
 }
 
+try {
+    $conn = mysqli_connect($hostname, $username, $password, $database, $port); 
+    $result = '데이터 베이스 접속 성공';
+} catch (PDOException $e) {
+    $result = '접속 실패';
+}
+
+// try {
+//     $pdo = new PDO('mysql:host='.$hostname.';port='.$port.';dbname='.$database.';charset=utf8', $username, $password);
+//     $result = '데이터 베이스 접속 성공';
+// } catch (PDOException $e) {
+//     $result = '접속 실패';
+// }
+
+echo $result;
+
 ?>
+
+
