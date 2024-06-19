f3157205d8990aab741181a4d2fcef29b41aad3c
Woonani <nada3377@yonsei.ac.kr>
2024-06-20 00:06:24 +0900

    잔 오류 수정

--- js/review/view.js
+++ js/review/view.js
@@ -8,50 +8,51 @@ document.addEventListener("DOMContentLoaded", () => {
   const lec_name = document.querySelector("#lec_name");
   const lec_info = document.querySelector("#lec_info");
 
-    const params = getQueryParams();
-    const seq = params.seq;
-
-   
-    let viewInfo;
+  const params = getQueryParams();
+  const seq = params.seq;
 
   // 리뷰 정보 하나 불러오는 함수
- fetch(`../../controller/review/view.php?mode=view&seq=${seq}`
-    ).then((res) => {
-        return res.json();
-    }).then((data) => {
-        console.log("data", data.data);
-        if (data.status) {
-          viewInfo =  data.data;
-          return viewInfo;
-          
-        } else {
-          console.log("데이터 조회 실패");
-          return false
-        }
-      })
+  fetch(`../../controller/review/view.php?mode=view&seq=${seq}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log("data", data.data);
+      if (data.status) {
+        const viewInfo = data.data;
+
+        rev_title.innerText = "제목 | " + viewInfo.rev_title;
+        user_id.innerText = "작성자 | " + viewInfo.user_id;
+        rating.innerText = viewInfo.rating;
+        contents.innerText = viewInfo.contents;
+        user_id2.innerText = viewInfo.user_id + "님의 수강하신 강의 정보";
+        image_url.src = viewInfo.image_url;
+        lec_name.innerText = viewInfo.lec_name;
+        lec_info.innerText =
+          "강사: " +
+          viewInfo.instructor +
+          " | 학습난이도 : " +
+          viewInfo.level +
+          " | 교육시간: " +
+          viewInfo.time +
+          "간 (" +
+          viewInfo.chapter +
+          "강)";
+      } else {
+        console.log("데이터 조회 실패");
+        return false;
+      }
+    })
     .catch((error) => console.error("Error:", error));
-  
-
-
-
-  rev_title.innerText = viewInfo.rev_title;
-  user_id.innerText = viewInfo.user_id;
-  rating.innerText = viewInfo.rating;
-  contents.innerText = viewInfo.contents;
-  user_id2.innerText = viewInfo.user_id2;
-  image_url.innerText = viewInfo.image_url;
-  lec_name.innerText = viewInfo.lec_name;
-  lec_info.innerText = viewInfo.lec_info;
 
   function getQueryParams() {
     const params = {};
     const queryString = window.location.search.slice(1);
-    const queries = queryString.split('&');
-    queries.forEach(query => {
-        const [key, value] = query.split('=');
-        params[key] = value;
+    const queries = queryString.split("&");
+    queries.forEach((query) => {
+      const [key, value] = query.split("=");
+      params[key] = value;
     });
     return params;
-}
-
+  }
 });
